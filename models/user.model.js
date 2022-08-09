d2729887b6d2b4dfb1d1732d553db57e75d5d196
Bill <billsong19@hotmail.com>
2022-08-09 20:51:25 +1200

    added backend

---
+++ models/user.model.js
@@ -0,0 +1,64 @@
+//user schema for mongodb
+import mongoose, { Schema, model } from "mongoose";
+
+const contact = new Schema({
+    title: { type: String, required: true },
+    name: { type: String, required: true },
+    email: String,
+    phone: { type: Number, required: true },
+});
+
+const reminder = new Schema({
+    title: { type: String, required: true },
+    start_date: { type: Date, required: true },
+    frequency: {
+        type: String,
+        required: true,
+        enum: ["once", "daily", "weekly", "monthly"],
+    },
+    description: String,
+    completed: { type: Boolean, default: false },
+});
+
+const symptomEntry = new Schema({
+    symptom: { type: String, required: true },
+    strength: {
+        type: String,
+        required: true,
+        enum: ["none", "mild", "moderate", "severe"],
+    },
+});
+
+const journalEntry = new Schema({
+    phys_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
+    ment_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
+    date: { type: Date, required: true },
+    symptoms: [symptomEntry],
+});
+
+const user = new Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    date_of_birth: { type: Date, required: true },
+    condition: {
+        cancer_type: {
+            type: String,
+            enum: [
+                "Bowel",
+                "Lung",
+                "Breast",
+                "Colon",
+                "Prostate",
+                "Bladder",
+                "Skin",
+                "Other",
+            ],
+        },
+        cancer_stage: { type: String },
+        last_update_date: { type: Date },
+        treatment_period: { type: Number },
+    },
+    contacts: [contact],
+});
+
+export const User = mongoose.model("User", user);
---
+++ routes/record.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const recordRoutes = express.Router();
