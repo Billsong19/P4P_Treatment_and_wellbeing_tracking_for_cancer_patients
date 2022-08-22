038beddafd38468e55c04b4f9d2797e5663ec607
Bill <billsong19@hotmail.com>
2022-08-22 18:17:44 +1200

    user endpoints implemented and working

--- routes/record.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
-const recordRoutes = express.Router();
