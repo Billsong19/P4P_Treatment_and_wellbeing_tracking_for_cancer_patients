4cd541b4530679a5154235e1ca6bb90e3311a026
Bill <billsong19@hotmail.com>
2022-09-27 01:14:01 +1300

    add note to schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,66 +1,72 @@
 //user schema for mongodb
 const { mongoose, Schema } = require("mongoose");
 
+const note = new Schema({
+  contents: String,
+  title: String,
+});
+
 const contact = new Schema({
-    title: String,
-    name: { type: String, required: true },
-    email: String,
-    phone: { type: Number, required: true },
+  title: String,
+  name: { type: String, required: true },
+  email: String,
+  phone: { type: Number, required: true },
 });
 
 const reminder = new Schema({
-    title: { type: String, required: true },
-    start_date: { type: Date, required: true },
-    frequency: {
-        type: String,
-        required: true,
-        enum: ["once", "daily", "weekly", "monthly"],
-    },
-    description: String,
-    completed: { type: Boolean, default: false },
+  title: { type: String, required: true },
+  start_date: { type: Date, required: true },
+  frequency: {
+    type: String,
+    required: true,
+    enum: ["once", "daily", "weekly", "monthly"],
+  },
+  description: String,
+  completed: { type: Boolean, default: false },
 });
 
 const symptomEntry = new Schema({
-    symptom: { type: String, required: true },
-    strength: {
-        type: String,
-        required: true,
-        enum: ["none", "mild", "moderate", "severe"],
-    },
+  symptom: { type: String, required: true },
+  strength: {
+    type: String,
+    required: true,
+    enum: ["none", "mild", "moderate", "severe"],
+  },
 });
 
 const journalEntry = new Schema({
-    phys_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
-    ment_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
-    date: { type: Date, required: true },
-    symptoms: [symptomEntry],
+  phys_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
+  ment_wlbing_rating: { type: Number, required: true, min: 1, max: 7 },
+  date: { type: Date, required: true },
+  symptoms: [symptomEntry],
 });
 
 const user = new Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    date_of_birth: { type: Date, required: true },
-    condition: {
-        cancer_type: {
-            type: String,
-            enum: [
-                "Bowel",
-                "Lung",
-                "Breast",
-                "Colon",
-                "Prostate",
-                "Bladder",
-                "Skin",
-                "Other",
-            ],
-        },
-        cancer_stage: { type: String },
-        last_update_date: { type: Date },
-        treatment_period: { type: Number },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  date_of_birth: { type: Date, required: true },
+  condition: {
+    cancer_type: {
+      type: String,
+      enum: [
+        "Bowel",
+        "Lung",
+        "Breast",
+        "Colon",
+        "Prostate",
+        "Bladder",
+        "Skin",
+        "Other",
+      ],
     },
-    contacts: [contact],
-    journal: [journalEntry],
-    reminders: [reminder],
+    cancer_stage: { type: String },
+    last_update_date: { type: Date },
+    treatment_period: { type: Number },
+  },
+  contacts: [contact],
+  journal: [journalEntry],
+  reminders: [reminder],
+  notes: [note],
 });
 
 module.exports = mongoose.model("User", user);
