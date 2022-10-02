1f40ec1f950784f2e4f792df730adef5299eb73b
Bill <billsong19@hotmail.com>
2022-10-03 10:52:24 +1300

    link hcpcontactscreen to backend

--- frontend/screens/HCPContactScreen.js
+++ frontend/screens/HCPContactScreen.js
@@ -2,24 +2,31 @@ import * as React from "react";
 import { Text, View, Pressable, ScrollView } from "react-native";
 import styles from "../styles";
 import { Linking } from "react-native";
+import { getUserContext } from "../components/UserContext";
 
 export const HCPContactScreen = ({ navigation }) => {
-    const DATA = [
-        { provider: "Dr. Smith, GP", phone: "123-456-7890" },
-        { provider: "Emergency Services", phone: "111" },
-    ];
-    const listItems = DATA.map((item) => (
-        <View style={[styles.wideTile, styles.blueBorder]}>
-            <Text>{item.provider}</Text>
-            <Text
-                style={{ color: "blue" }}
-                onPress={() => {
-                    Linking.openURL(`tel:${item.phone}`);
-                }}
-            >
-                {item.phone}
-            </Text>
-        </View>
-    ));
-    return listItems;
+  const { user } = getUserContext();
+
+  function userIsNull() {
+    return user == null;
+  }
+
+  const listItems = userIsNull() ? (
+    <Text> loading... </Text>
+  ) : (
+    user.contacts.map((item, index) => (
+      <View key={index} style={[styles.wideTile, styles.blueBorder]}>
+        <Text>{`${item.title} ${item.name}`}</Text>
+        <Text
+          style={{ color: "blue" }}
+          onPress={() => {
+            Linking.openURL(`tel:${item.phone}`);
+          }}
+        >
+          {item.phone}
+        </Text>
+      </View>
+    ))
+  );
+  return listItems;
 };
